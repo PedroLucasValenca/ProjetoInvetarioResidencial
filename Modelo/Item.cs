398cd0f6fe8e4ed9e1e2e9c803c7708bc8ebbe1d
PedroLucasValenca <39070022+PedroLucasValenca@users.noreply.github.com>
2018-06-11 21:27:15 -0300

    Criando Versao Inicial mais Modelo

---
+++ Modelo/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+
+        public String  NomeItem { get; set; }
+
+        public int Quantidade { get; set; }
+
+        public String Descricao { get; set; }
+
+        public int LocalID { get; set; }
+
+        public int LocalUsoID { get; set; }
+    }
+}
---
+++ Modelo/Local.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+     public class Local
+    {
+        public int LocalID { get; set; }
+
+        public String NomeLocal { get; set; }
+
+
+    }
+}
