c05c3c39cfce03419bbd861152760c67ff7e6b68
PedroLucasValenca <39070022+PedroLucasValenca@users.noreply.github.com>
2018-06-18 22:00:41 -0300

    Criando a ItensController implementando interface IBaseController

---
+++ Controllers/ItensController.cs
@@ -0,0 +1,38 @@
+﻿using Controllers.Base;
+using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    public class ItensController : IBaseController<Item>
+    {
+        public void Adicionar(Item entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item BuscarPorID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Editar(Item entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Item> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
