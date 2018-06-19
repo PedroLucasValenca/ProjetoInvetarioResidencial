b8d251073c37b62cbc17544c151c891610828186
PedroLucasValenca <39070022+PedroLucasValenca@users.noreply.github.com>
2018-06-18 21:24:30 -0300

    Instalando o EntityFramework nas soluções e criando classe Contexto

---
+++ Controllers/DAL/Contexto.cs
@@ -0,0 +1,24 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers.DAL
+{
+    class Contexto : DbContext
+    {
+        public Contexto() : base("strConn")
+        {
+
+        }
+
+        public DbSet<Item> Items { get; set; }
+
+        public DbSet<Local> Locais { get; set; }
+
+        public DbSet<TipoUso> TiposUsos { get; set; }
+    }
+}
