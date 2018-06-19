5c51ec30d2b7c5aea0adc486c70f89572bb2abe7
PedroLucasValenca <39070022+PedroLucasValenca@users.noreply.github.com>
2018-06-18 21:07:09 -0300

    Criando Controllers e criando interface de implementação

---
+++ Controllers/Base/IBaseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers.Base
+{
+    interface IBaseController<P> where P : class
+    {
+        void Adicionar(P entity);
+
+        void Editar(P entity);
+
+        void Excluir(int id);
+
+        IList<P> ListarTodos();
+
+        P BuscarPorID(int id);
+
+
+    }
+}
