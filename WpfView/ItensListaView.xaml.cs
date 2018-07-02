4751dd0b49b0c95eb12b3955b7d08f27f7de8e92
PedroLucasValenca <39070022+PedroLucasValenca@users.noreply.github.com>
2018-07-01 23:46:52 -0300

    Criando ItensListaView e inserindo dataGrid

---
+++ WpfView/ItensListaView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfView
+{
+    /// <summary>
+    /// Lógica interna para ItensListaView.xaml
+    /// </summary>
+    public partial class ItensListaView : Window
+    {
+        public ItensListaView()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ItensController itensControler = new ItensController();
+
+            ItemDataGrid.ItemsSource = itensControler.ListarTodos();
+        }
+    }
+}
