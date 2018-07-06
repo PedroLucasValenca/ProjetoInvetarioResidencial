77618095b5db4d2cf3bec8127aaa61bb8a4fc5ac
PedroLucasValenca <39070022+PedroLucasValenca@users.noreply.github.com>
2018-07-06 01:23:25 -0300

    Excluir locação

--- WpfView/EditarLocacaoView.xaml.cs
+++ WpfView/EditarLocacaoView.xaml.cs
@@ -51,31 +51,56 @@ namespace WpfView
 
         private void CriarLocacaoButton_Click(object sender, RoutedEventArgs e)
         {
+            LocacaoView locacaoView = new LocacaoView();
 
+            locacaoView.ShowDialog();
         }
 
         private void ListarLocacaoButton_Click(object sender, RoutedEventArgs e)
         {
+            ListarLocacaoView listarLocacaoView = new ListarLocacaoView();
 
+            listarLocacaoView.ShowDialog();
         }
 
         private void CriarTipoUsoButton_Click(object sender, RoutedEventArgs e)
         {
+            TipoUsoView tipoUsoView = new TipoUsoView();
 
+            tipoUsoView.ShowDialog();
         }
 
         private void ListarTipoUsoButton_Click(object sender, RoutedEventArgs e)
         {
+            TipoUsoListaView tipoUsoListaView = new TipoUsoListaView();
 
+            tipoUsoListaView.ShowDialog();
         }
 
         private void CriarItemButton_Click(object sender, RoutedEventArgs e)
         {
+            ItemView itemView = new ItemView();
 
+            itemView.ShowDialog();
         }
 
         private void ListarItemButton_Click(object sender, RoutedEventArgs e)
         {
+            ItensListaView itensListaView = new ItensListaView();
+
+            itensListaView.ShowDialog();
+        }
+
+        private void ExcluirLocacaoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Local local = new Local();
+            local.LocalID = int.Parse(EditaIDLocacaoLabel.Text);
+
+            LocaisController locaisController = new LocaisController();
+
+            locaisController.Excluir(local.LocalID);
+
+            this.Close();
 
         }
     }
