a54de18e5800d58b0e103c00b1137bd2426d65c2
PedroLucasValenca <39070022+PedroLucasValenca@users.noreply.github.com>
2018-07-05 21:01:38 -0300

    Criando Tela de Editar locacao

---
+++ WpfView/EditarLocacaoView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfView
+{
+    /// <summary>
+    /// Lógica interna para EditarLocacaoView.xaml
+    /// </summary>
+    public partial class EditarLocacaoView : Window
+    {
+        public EditarLocacaoView()
+        {
+            InitializeComponent();
+        }
+
+        private void EditaLocacaoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
